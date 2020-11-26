d8027ec624817dc57cf44cd515887bba6af73844
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-11-26 10:53:07 -0800

    feat(redux): add redux
    added redux and created initial values for getting post value

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import {rootReducer} from './redux/index';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <Provider store={store}>
+      <BrowserRouter>
+         <React.StrictMode>
+            <App />
+         </React.StrictMode>
+      </BrowserRouter>
+   </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/redux/index.tsx
@@ -0,0 +1,7 @@
+import {combineReducers} from 'redux';
+import {example} from './reducer/remp';
+
+
+export const rootReducer = combineReducers({
+   example
+})
