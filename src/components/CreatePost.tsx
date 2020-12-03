a6a64fb724bd2484ffebbbcd5c8e1b26cfbde7d6
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-12-02 17:17:25 -0800

    feat(create-post): user wont be able to post unless they are signed in
    added user id to localstorage and added a simple statement to check if we have the id. if we do then we let user add post

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { SetStateAction } from 'react';
 import {useDispatch} from 'react-redux';
 import {useForm} from 'react-hook-form';
 import {CREATE_POST} from '../redux/actions/actions';
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 interface OnSubmitProps {
    post: string;
@@ -13,14 +15,27 @@ interface Props {
 }
 
 const CreatePost: React.FC<Props> = ({getPostData}) => {
+   const [userId, setUserId] = useState<SetStateAction<string> | null>();
    const {handleSubmit, register, reset } = useForm();
    const dispatch = useDispatch();
 
+   useEffect(() => {
+      const getUserId = localStorage.getItem("user_id");
+      if(getUserId) {
+         setUserId(getUserId);
+      }
+   },[]);
+
    const onSubmit = async (value: OnSubmitProps) => {
       // dispatch({type: CREATE_POST, payload: value.post});
-      await axios.post(`${process.env.REACT_APP_API_SERVER_URL}/post/create/${4}`, value);
-      reset();
-      getPostData();
+      if(userId) {
+         await axios.post(`${process.env.REACT_APP_API_SERVER_URL}/post/create/${userId}`, value);
+         reset();
+         getPostData();
+      } else {
+         alert("Please login first");
+      }
+
    };
 
    return (
